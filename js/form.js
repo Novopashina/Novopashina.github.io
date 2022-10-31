c8a5a20fce31af633b37a3f7c0c5abb588143214
novopashina <112856474+Novopashina@users.noreply.github.com>
2022-10-31 21:24:30 +0300

    Delete form.js

--- js/form.js
+++
@@ -1,7 +0,0 @@
-
-function click1() {
-  let f1 = document.getElementsByName("cena");
-  let f2 = document.getElementsByName("kolichestvo");
-  let r = document.getElementById("result");
-  r.innerHTML = f1[0].value * f2[0].value;
-}
